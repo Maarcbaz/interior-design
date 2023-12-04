f76d1c0dd4dcda3397af2f5c4d2e2e01e94a289b
Maarcbaz <138406453+Maarcbaz@users.noreply.github.com>
2023-12-04 01:09:04 +0100

    initial commit

---
+++ app..js
@@ -0,0 +1,38 @@
+const nav = document.getElementById('topNav')
+const header = document.getElementById('logo')
+const list = document.getElementById('list')
+
+function openNav() {
+    nav.style.width = '250px';
+    list.style.display = 'block'
+    header.style.display = 'block'
+
+
+}
+
+function closeNav() {
+    nav.style.width = '0'
+    list.style.display = 'none'
+    
+    header.style.display = 'none'
+
+    list.style.display = 'none'
+
+}
+
+const overlay = document.getElementById('myNav')
+const hList = document.getElementById('hList')
+
+function openNa() {
+    overlay.style.width = '250px';
+    hList.style.display = 'block'
+}
+
+function closeNa() {
+    overlay.style.width = '0'
+    hList.style.display = 'none'
+    
+
+    hList.style.display = 'none'
+
+}
